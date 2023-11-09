da0355d3fa70bac77a979453c53555df15c65563
Veerle van Leemput <info@hypebright.nl>
2023-11-09 10:34:38 +0100

    remove browser comments

--- www/scrollPage.js
+++ www/scrollPage.js
@@ -13,6 +13,6 @@ function scrollFunction() {
 
 // Handle the click event on the scroll-to-top button
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 };
