a5d13278a3000b397688a2c334f37211f8cc913b
Veerle van Leemput <info@hypebright.nl>
2024-02-16 16:46:59 +0100

    📝 add more context - closes #1

--- www/scrollPage.js
+++ www/scrollPage.js
@@ -1,5 +1,8 @@
 // Modfied from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
+// Note that you can also omit the scrollFunction() and directly write
+// window.onscroll = function () { //JS stuff }
+// or use a ternary operator and arrow function.
+// See https://github.com/hypebright/scroll-to-top-shiny/issues/1
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
